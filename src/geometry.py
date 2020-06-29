958c3f9b5a85ae3d700297c400e5e4fc65b98655
MaijaYlosmaki <maija.ylosmaki@gmail.com>
2020-06-30 01:21:09 +0300

    added json training - a dictionary -kind

--- src/geometry.py
+++ src/geometry.py
@@ -4,12 +4,12 @@ __author__ = "M.Ylösmäki"
 
 import math
 
-def hypothenuse(a, b):
+def triangle_hypothenuse(a, b):
     "returns the length of the hypothenuse when given the lengths of two other sides (a and b) of a right-angled triangle"
     c = math.sqrt(a**2 + b**2)
     return c
 
-def area(a, b):
+def triangle_area(a, b):
     "returns the area of the right-angled triangle, when two sides (a and b), perpendicular to each other, are given as parameters"
     #or something
     c = (a * b) / 2
